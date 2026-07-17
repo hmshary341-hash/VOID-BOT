c3b8809da4c090d727af864753caf2720a0d5c12
hmshary341-hash <hmshary341@gmail.com>
2026-07-18 02:54:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("VOID BOT")
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+TOKEN = os.getenv("TOKEN")
+bot.run(TOKEN)
