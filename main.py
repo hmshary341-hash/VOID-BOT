3389a9801de7fdefe3a2bd6ef1192d953f270613
hmshary341-hash <hmshary341@gmail.com>
2026-07-18 03:51:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,22 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+CHANNEL_ID = 1526823698089119784
+SEPARATOR_IMAGE = "https://files.catbox.moe/7q1g3v.png"
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.channel.id == CHANNEL_ID:
+        await message.channel.send(SEPARATOR_IMAGE)
+
+    await bot.process_commands(message)
 
 @bot.command()
 async def ping(ctx):
