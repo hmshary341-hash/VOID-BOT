9dbb54c85ad13afb60c75c4e885768147fcad17a
hmshary341-hash <hmshary341@gmail.com>
2026-07-19 20:26:52 +0300

    Update colors.py

--- cogs/colors.py
+++ cogs/colors.py
@@ -21,6 +21,7 @@ class Colors(commands.Cog):
             12: 1527768986115244253,
         }
 
+    # 1. أمر اختيار اللون
     @app_commands.command(name="color", description="اختر لونك الخاص برقم")
     @app_commands.describe(number="رقم اللون (من 1 إلى 12)")
     async def color(self, interaction: discord.Interaction, number: int):
@@ -51,6 +52,19 @@ class Colors(commands.Cog):
             await interaction.response.send_message("❌ ليس لدي صلاحية لتعديل رتبك (تأكد أن رتبة البوت أعلى من رتبة اللون).", ephemeral=True)
         except Exception as e:
             await interaction.response.send_message(f"❌ حدث خطأ غير متوقع: {e}", ephemeral=True)
+
+    # 2. أمر عرض لوحة الألوان
+    @app_commands.command(name="show_colors", description="عرض لوحة الألوان")
+    async def show_colors(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            title="🎨 Colors Picker",
+            description="اختر لونك المفضل من الأرقام أدناه باستخدام الأمر `/color`",
+            color=discord.Color.blurple()
+        )
+        # الصورة المدمجة
+        embed.set_image(url="https://cdn.discordapp.com/attachments/1513801918374215741/1528452532832174282/image0.jpg?ex=6a5e59d9&is=6a5d0859&hm=3b4ab87b25c85e4a9687f455a1b172ee0e298a7c20e138ace2fb02191ac5dbd9&") 
+        
+        await interaction.response.send_message(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Colors(bot))
