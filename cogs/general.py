4bfa80562806ad9f628b9f518930a7975e758485
hmshary341-hash <hmshary341@gmail.com>
2026-07-19 08:15:19 +0300

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -5,12 +5,5 @@ class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # هنا ضع أي أوامر أخرى لديك في الملف (مثل أوامر المعلومات أو الترحيب إلخ...)
-    # تأكد فقط أنك حذفت أي شيء مكتوب فيه @app_commands.command(name="clear")
-    
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print(f"General Cog loaded.")
-
 async def setup(bot):
     await bot.add_cog(General(bot))
